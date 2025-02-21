9fd0fdb50ddc62b45e458d4d357e626af9d51183
Gaurav <gaurav.bhe.24@gmail.com>
2025-02-21 21:43:28 +0530

    like service.js updated

--- src/services/like-service.js
+++ src/services/like-service.js
@@ -1,63 +1,37 @@
 
-import { LikeRepository, TweetRepository } from '../repository/index.js';
+import { LikeRepository } from '../repository/index.js';
 
-// console.log('likes is', like)
 class LikeService {
     constructor() {
         this.likeRepository = new LikeRepository();
-        this.tweetRepository = new TweetRepository();
     }
 
-    async toggleLike(modelId, modelType, userId) { // /api/v1/likes/toggle?id=modelid&type=Tweet
-        console.log(modelId, modelType, userId);
-        if(modelType == 'Tweet') {
-            var likeable = await this.tweetRepository.find(modelId);
-
-           
-
-        } else if(modelType == 'Comment') {
-            //ToDo
-
-        } else {
-            throw new Error('unknown model type');
-        }
-// there is a logic about like:
- // if user already liked then removed like if user first time like then like
-        const exists = await this.likeRepository.findByUserAndLikeable({
+    async toggleLike(likeable, modelType, userId) {
+        // Find if the user has already liked this model (Tweet or Comment)
+        const existingLike = await this.likeRepository.findByUserAndLikeable({
             user: userId,
             onModel: modelType,
-            likeable: modelId
+            likeable: likeable._id // Use the _id directly from the fetched document
         });
 
-
-       
-        console.log('exists', typeof(exists));
-         if(exists) {
-            likeable.likes.pull(exists.id); // uncomment it #problem
+        if (existingLike) {
+            // Remove the like
+            likeable.likes.pull(existingLike.id);
             await likeable.save();
-            await exists.deleteOne();       // uncomment it  #Problem
-            
-
-            var isAdded = false;
-
-         }else {
+            await existingLike.deleteOne(); // Remove like from the database
+            return { isLiked: false };
+        } else {
+            // Add new like
             const newLike = await this.likeRepository.create({
                 user: userId,
                 onModel: modelType,
-                likeable: modelId
+                likeable: likeable._id // Use _id from the likeable object
             });
-             // Add the like
-            likeable.likes.push(newLike);    // 1 error this is a error will solve it
-            // Save the updated tweet
+            likeable.likes.push(newLike); // Add to tweet/comment's likes
             await likeable.save();
-
-            var isAdded = true;
-         }
-         return isAdded;
-    
-
-}
-
+            return { isLiked: true };
+        }
+    }
 }
 
 export default LikeService;
