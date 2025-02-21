1783f763e04044ed06c7b8b007d43e0241b19a8e
Gaurav <gaurav.bhe.24@gmail.com>
2025-02-21 21:41:30 +0530

    like model setup to handle like funct with datbase

--- src/models/like.js
+++ src/models/like.js
@@ -1,19 +1,12 @@
 import mongoose from 'mongoose';
 
-// there is a structure of following coding
-// like model is ready
-// this is areal writing schema
 const likeSchema = new mongoose.Schema({
-    // on which model we are going to like
-    // or we are going to like on multiple tweet or like on comment
     onModel: {
         type: String,
         required: true,
         enum: ['Tweet', 'Comment']
     },
     likeable: {
-        // whenever we like a comment or like a tweet we need to store their id
-        // if like on tweet, tweet id will store, if comment, comment id will store
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         refPath: 'onModel'
@@ -23,7 +16,12 @@ const likeSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     }
-}, {Timestamps: true});
+}, { timestamps: true });  // 
 
 const Like = mongoose.model('Like', likeSchema);
 export default Like;
+
+
+
+
+
