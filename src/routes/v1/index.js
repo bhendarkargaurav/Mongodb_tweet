72aae4483e1465f12e45ddb7bc15db6d7012d374
Gaurav <gaurav.bhe.24@gmail.com>
2025-02-21 21:47:29 +0530

    router manged

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -11,11 +11,12 @@ import { authenticate } from '../../middlewares/authenticate.js'
 const router = express.Router();
 
 router.post('/tweets', createTweet);
+
 router.get('/tweets/:id', getTweet);
 
 router.post('/likes/toggle',toggleLike);
 
-router.post('/comments', authenticate, createComment);
+router.post('/comments', createComment);
 
 router.post('/signup', signup);
 
@@ -23,13 +24,3 @@ router.post('/login', login);
 
 export default router;
 
-/**
- * 
- * user -> unique_id -
- * 
- * https://www.mywebsite.com/verifyEmail/skhvbv8w874rte4t
- * 
- * verifyEmail/:unique_id -> decrypttoken (unique_id) -> {id: wi4uy, email: w8ery}
- * 
- * db -> userid -> unique_id -> createdAt -> isVerified
- */
