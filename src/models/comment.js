723ab3797908c4839df8b6796515eede43cc56c6
Gaurav <gaurav.bhe.24@gmail.com>
2025-02-24 22:07:19 +0530

    modification in mode

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,8 +1,4 @@
-// The commentSchema you’ve provided defines a MongoDB schema using Mongoose for a Comment
-// model. This schema is designed to handle a flexible and nested commenting system where 
-//comments can be made either on a tweet or on another comment. 
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
     content: {
@@ -12,28 +8,25 @@ const commentSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        require: true
+        required: true
     },
-    onmodel: {  // onmodel tell us that comment was made on a tweet and comment was maide on a comment
+    onModel: {
         type: String,
         required: true,
-        enum: ['Tweet', 'Comment']// comnment on a tweet or comment on a comment only
-    }, 
-    commentable: {// like likeable
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         refPath: 'onModel'
     },
-  
-    //we are putting comments in tweet.js and comment.js, so we need to manage it differntly
     comments: [
-        {   // storing comment like likes are stored in tweet.js
+        {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Comment',
-        } 
+        }
     ]
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
 export default Comment;
-
